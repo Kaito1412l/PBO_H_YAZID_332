2bef68c17adb5578d0e1253269a9953efc4992bc
Kaito1412l <kaito141226l@gmail.com>
2025-03-07 08:33:13 +0700

    Update kegiatan 1

--- Tugas/Kegiatan_1.java
+++ Tugas/Kegiatan_1.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class Kegiatan_1 {
-    public static void main(String[] args) {
-        //Menyatakan Variabel Final/CONST Untuk Username dan Password
-        final String kuadmin = "admin332";
-        final String kpadmin = "password332";
-        final String kusiswa = "Nur Muhammad Yazid Salim";
-        final String kpsiswa = "202410370110332";
+    //Menyatakan Variabel Final/CONST Untuk Username dan Password
+    final String kuadmin = "admin332";
+    final String kpadmin = "password332";
+    final String kusiswa = "Nur Muhammad Yazid Salim";
+    final String kpsiswa = "202410370110332";
 
+    public static void main(String[] args) {
+        //inisialisasi untuk memanggil kegiatan 1
+        Kegiatan_1 program = new Kegiatan_1();
+        program.runProgram();}
+
+    public void runProgram() {
         //Memanggil Scanner
         Scanner scan = new Scanner(System.in);
 
         //Menampilkan Tampilan Awal atau Pilihan Program Login
-        String user, pass;
         System.out.println("==========Program Dimulai==========");
         System.out.println("Pilih Login:");
         System.out.println("1. Admin");
@@ -26,38 +30,41 @@ public class Kegiatan_1 {
 
         //Memproses Input Pilihan
         switch (pilihan){
-            case 1:
-                //Memproses Pilihan Login Admin
-                System.out.print("Masukkan Username: ");
-                user = scan.nextLine();
-                System.out.print("Masukkan Password: ");
-                pass = scan.nextLine();
-                //Melakukan Proses Validasi/Pengecekan Username dan Password
-                if ((kuadmin.equals(user)) && (kpadmin.equals(pass)))
-                   {System.out.println("\nLogin Admin Berhasil");}
-                else
-                   {System.out.println("\nLogin Gagal! Username atau Password Salah");}
-                break;
-            case 2:
-                //Memproses Pilihan Login Mahasiswa
-                System.out.print("Masukkan Nama: ");
-                user = scan.nextLine();
-                System.out.print("Masukkan NIM: ");
-                pass = scan.nextLine();
-                //Melakukan Proses Validasi/Pengecekan Username dan Password
-                if ((kusiswa.equals(user)) && (kpsiswa.equals(pass)))
-                   {//Menampilkan Data Login Mahasiswa
-                    System.out.println("\nLogin Mahasiswa Berhasil");
-                    System.out.println("Nama: " + kusiswa);
-                    System.out.println("NIM: " + kpsiswa);}
-                else
-                   {System.out.println("\nLogin Gagal! Nama atau NIM Salah");}
-                break;
-        default:
-            //Menyatakan Input Tidak ada Di Dalam Pilihan
-            System.out.println("\nPilihan tidak valid"); break;}
+            case 1 -> loginAdmin(scan);
+            case 2 -> loginMahasiswa(scan);
+            default ->System.out.println("\nPilihan tidak valid");}
 
         //Memulangkan Scanner
-        scan.close();
-        }
+        scan.close();}
+
+    //Method untuk login admin
+    private void loginAdmin(Scanner scan) {
+        //Memproses Pilihan Login Admin
+        System.out.print("Masukkan Username: ");
+        String user = scan.nextLine();
+        System.out.print("Masukkan Password: ");
+        String pass = scan.nextLine();
+
+        //Melakukan Proses Validasi/Pengecekan Username dan Password
+        if ((kuadmin.equals(user)) && (kpadmin.equals(pass)))
+           {System.out.println("\nLogin Admin Berhasil");}
+        else
+           {System.out.println("\nLogin Gagal! Username atau Password Salah");}}
+
+    //Method untuk login mahasiswa
+    private void loginMahasiswa(Scanner scan) {
+        //Memproses Pilihan Login Mahasiswa
+        System.out.print("Masukkan Nama: ");
+        String user = scan.nextLine();
+        System.out.print("Masukkan NIM: ");
+        String pass = scan.nextLine();
+
+        //Melakukan Proses Validasi/Pengecekan Username dan Password
+        if ((kusiswa.equals(user)) && (kpsiswa.equals(pass)))
+           {//Menampilkan Data Login Mahasiswa
+            System.out.println("\nLogin Mahasiswa Berhasil");
+            System.out.println("Nama: " + kusiswa);
+            System.out.println("NIM: " + kpsiswa);}
+        else
+           {System.out.println("\nLogin Gagal! Nama atau NIM Salah");}}
     }
