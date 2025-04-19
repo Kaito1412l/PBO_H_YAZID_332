50f9b8e84f897ad7605c81f48124b8e205cba67c
Kaito1412l <kaito141226l@gmail.com>
2025-04-19 09:58:03 +0700

    Update Histori

--- Tugas/TUGAS_PBO/src/loginSystem.java
+++ Tugas/TUGAS_PBO/src/loginSystem.java
@@ -1,39 +1,70 @@
 import java.util.Scanner;
 
-public class loginSystem {
+public class LoginSystem {
+    //Menyatakan Variabel Final/CONST Untuk Username dan Password
+    final String kuadmin = "admin332";
+    final String kpadmin = "password332";
+    final String kusiswa = "Nur Muhammad Yazid Salim";
+    final String kpsiswa = "202410370110332";
+
     public static void main(String[] args) {
         //inisialisasi untuk memanggil kegiatan 1
-        loginSystem program = new loginSystem();
-        program.runProgram();
-    }
+        LoginSystem program = new LoginSystem();
+        program.runProgram();}
 
     public void runProgram() {
-        //Memanggil Class Admin Dan Mahasiswa
-        admin Admin = new admin();
-        mahasiswa Mahasiswa = new mahasiswa();
         //Memanggil Scanner
         Scanner scan = new Scanner(System.in);
 
         //Menampilkan Tampilan Awal atau Pilihan Program Login
-        displayPilihan();
+        System.out.println("==========Program Dimulai==========");
+        System.out.println("Pilih Login:");
+        System.out.println("1. Admin");
+        System.out.println("2. Mahasiswa");
 
         //Meminta Input Pilihan Login
         System.out.print("Masukkan Pilihan: ");
         int pilihan = scan.nextInt();
+        //Membersihkan Buffer Input pada Scanner
+        scan.nextLine();
 
         //Memproses Input Pilihan
-        switch (pilihan) {
-            case 1 -> Admin.login();
-            case 2 -> Mahasiswa.login();
-            default -> System.out.println("\nPilihan tidak valid");}
+        switch (pilihan){
+            case 1 -> loginAdmin(scan);
+            case 2 -> loginMahasiswa(scan);
+            default ->System.out.println("\nPilihan tidak valid");}
 
         //Memulangkan Scanner
         scan.close();}
 
-    //Method Untuk Pilihan Login
-    void displayPilihan(){
-        System.out.println("\n==========Program Dimulai==========\n");
-        System.out.println("Pilihan Login:");
-        System.out.println("1. Admin");
-        System.out.println("2. Mahasiswa");}
+    //Method untuk login admin
+    private void loginAdmin(Scanner scan) {
+        //Memproses Pilihan Login Admin
+        System.out.print("Masukkan Username: ");
+        String user = scan.nextLine();
+        System.out.print("Masukkan Password: ");
+        String pass = scan.nextLine();
+
+        //Melakukan Proses Validasi/Pengecekan Username dan Password
+        if ((kuadmin.equals(user)) && (kpadmin.equals(pass)))
+        {System.out.println("\nLogin Admin Berhasil");}
+        else
+        {System.out.println("\nLogin Gagal! Username atau Password Salah");}}
+
+    //Method untuk login mahasiswa
+    private void loginMahasiswa(Scanner scan) {
+        //Memproses Pilihan Login Mahasiswa
+        System.out.print("Masukkan Nama: ");
+        String user = scan.nextLine();
+        System.out.print("Masukkan NIM: ");
+        String pass = scan.nextLine();
+
+        //Melakukan Proses Validasi/Pengecekan Username dan Password
+        if ((kusiswa.equals(user)) && (kpsiswa.equals(pass)))
+        {//Menampilkan Data Login Mahasiswa yang login
+            System.out.println("\nLogin Mahasiswa Berhasil");
+            System.out.println("Nama: " + kusiswa);
+            System.out.println("NIM: " + kpsiswa);}
+        else
+        {System.out.println("\nLogin Gagal! Nama atau NIM Salah");}}
 }
